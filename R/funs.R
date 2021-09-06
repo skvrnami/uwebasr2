ee31e4a194837a7be319daa8fe2bdeb7b07591fe
Michael Škvrňák <397925@mail.muni.cz>
2021-09-06 17:47:13 +0200

    Cleanup funs file

--- R/funs.R
+++ R/funs.R
@@ -1,17 +1,3 @@
-# Hello, world!
-#
-# This is an example function named 'hello'
-# which prints 'Hello, world!'.
-#
-# You can learn more about package authoring with RStudio at:
-#
-#   http://r-pkgs.had.co.nz/
-#
-# Some useful keyboard shortcuts for package authoring:
-#
-#   Install Package:           'Cmd + Shift + B'
-#   Check Package:             'Cmd + Shift + E'
-#   Test Package:              'Cmd + Shift + T'
 
 #' Post audio file to UWebASR speech recognition engine and retrieve transcription
 #'
@@ -25,7 +11,6 @@ post_uwebasr <- function(lang_model = "CZ",
                body = httr::upload_file(path_file)) -> out
 
     stringr::str_conv(out$content)
-
 }
 
 #' Get audio transcription from UWebASR speech recognition engine
